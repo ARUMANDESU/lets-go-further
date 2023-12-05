17c0015b01156519791a007889e1c1271224031d
arumandesu <zh.arumandes@gmail.com>
2023-12-05 15:24:02 +0600

    text -> json error messages

--- cmd/api/router.go
+++ cmd/api/router.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (app *application) routes() *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.NotFound(app.notFoundResponse)
+	r.MethodNotAllowed(app.methodNotAllowedResponse)
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/healthcheck", app.healthcheckHandler)
