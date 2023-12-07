a061e3db30dbaf8c73446ad76ebce259dee56531
arumandesu <zh.arumandes@gmail.com>
2023-12-07 20:43:51 +0600

    DB connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +17,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -22,14 +28,27 @@ type application struct {
 }
 
 func main() {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	err := godotenv.Load()
+	if err != nil {
+		logger.Fatal("Loading .env file")
+	}
 
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production")
+	flag.StringVar(&cfg.db.dsn, "dsn", os.Getenv("DATABASE_DSN"), "PostgreSQL DSN")
+
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		config: cfg,
@@ -45,7 +64,24 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
 
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	db, err := pgxpool.New(context.Background(), cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.Ping(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
